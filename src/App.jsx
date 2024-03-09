0b7fb6553edd0f8f9fa0a2e284136bae6ebd1f34
Andrés Martínez <sbsmrth>
2024-03-08 22:33:07 -0500

    feat: side location effect managed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { useRef, useState } from 'react';
-
+import { useRef, useState, useEffect } from 'react';
 import { Places } from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import { Modal } from './components/Modal.jsx';
 import { DeleteConfirmation } from './components/DeleteConfirmation.jsx';
+import { sortPlacesByDistance } from './loc.js';
 import logoImg from './assets/logo.png';
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
+  const [availablePlaces, setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        latitude,
+        longitude
+      );
+
+      setAvailablePlaces(sortedPlaces);
+    });
+  }, []);
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -63,7 +77,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText="Sorting places by distance"
           onSelectPlace={handleSelectPlace}
         />
       </main>
